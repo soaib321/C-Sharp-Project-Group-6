9968da2d3845ffd51a7b63fe11790dd8104c6720
Tonni <tonnismrity.ts@gmail.com>
2026-01-16 23:30:34 +0600

    Button reorder

--- WindowsFormsApp1/allreq.Designer.cs
+++ WindowsFormsApp1/allreq.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(600, 22);
+            this.button1.Location = new System.Drawing.Point(330, 321);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
